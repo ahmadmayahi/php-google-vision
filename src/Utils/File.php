b8ecc1d581d0778fbaa675a938a963c2ee6e6d87
Ahmad Mayahi <am@ordrestyring.dk>
2021-09-24 13:38:31 +0200

    Accept different file types

--- src/Utils/File.php
+++ src/Utils/File.php
@@ -2,38 +2,91 @@
 
 namespace AhmadMayahi\GoogleVision\Utils;
 
-use AhmadMayahi\GoogleVision\Contracts\File;
+use AhmadMayahi\GoogleVision\Config;
+use AhmadMayahi\GoogleVision\Contracts\File as FileContract;
+use AhmadMayahi\GoogleVision\Exceptions\FileNotFoundException;
 use Exception;
-use Generator;
 use SplFileInfo;
+use SplFileObject;
 
-class LocalFile extends SplFileInfo implements File
+class File implements FileContract
 {
     /**
-     * @throws Exception
+     * @param string|resource|SplFileInfo|SplFileObject $file
+     *
      */
-    public function getFileContents(): string
+    public function __construct(private $file, private Config $config)
     {
-        if (false === $this->isReadable()) {
-            throw new Exception('File is not readable!');
-        }
-
-        $fileObj = $this->openFile();
-
-        return $fileObj->fread($this->getSize());
     }
 
-    public function __toString()
+    /**
+     * @return resource|string
+     *
+     * @throws FileNotFoundException
+     */
+    public function toGoogleVisionFile()
     {
-        
+        if ($this->isGoogleStorage()) {
+            /** @var string $file */
+            $file = $this->file;
+
+            return $file;
+        }
+
+        if (is_string($this->file)) {
+            /** @var string $file */
+            $file = file_get_contents($this->file);
+
+            return $file;
+        }
+
+        if (is_resource($this->file)) {
+            return $this->file;
+        }
+
+        if ($this->file instanceof SplFileInfo) {
+            $fileObj = $this->file->openFile();
+
+            /** @var string $string */
+            $file = $fileObj->fread($this->file->getSize());
+
+            return $file;
+        }
+
+        throw new FileNotFoundException('File not found!');
     }
 
-    public function readFileUsingGenerator(): Generator
+    public function getLocalPathname()
     {
-        $handle = $this->openFile();
-
-        while (false === $handle->eof()) {
-            yield $handle->fgets();
+        if ($this->isGoogleStorage()) {
+            throw new Exception('Google Storage is not supported for this operation!');
         }
+
+        if (is_string($this->file)) {
+            return $this->file;
+        }
+
+        if (is_resource($this->file)) {
+            return $this->resourceTemp();
+        }
+
+        if (($this->file instanceof SplFileInfo) || ($this->file instanceof SplFileObject)) {
+            return $this->file->getPathname();
+        }
+
+        throw new Exception('Cannot get the local file path');
+    }
+
+    private function resourceTemp(): string
+    {
+        $tempFile = tempnam($this->config->getTempDirPath(), sha1(uniqid()));
+        file_put_contents($tempFile, stream_get_contents($this->file));
+
+        return $tempFile;
+    }
+
+    public function isGoogleStorage(): bool
+    {
+        return is_string($this->file) && str_starts_with($this->file, 'gs://');
     }
 }
