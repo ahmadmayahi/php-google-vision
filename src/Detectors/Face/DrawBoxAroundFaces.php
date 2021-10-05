92ef0774a484d872daebad9573d2eaf1367c19a2
Ahmad Mayahi <am@ordrestyring.dk>
2021-10-05 15:22:48 +0200

    Update face

---
+++ src/Detectors/Face/DrawBoxAroundFaces.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AhmadMayahi\Vision\Detectors\Face;
+
+use AhmadMayahi\Vision\Contracts\Drawable;
+use AhmadMayahi\Vision\Data\FaceData;
+use AhmadMayahi\Vision\Detectors\Face;
+use AhmadMayahi\Vision\Enums\ColorEnum;
+use AhmadMayahi\Vision\Utils\Image;
+
+class DrawBoxAroundFaces implements Drawable
+{
+    private int $borderColor = ColorEnum::GREEN;
+
+    public function __construct(private Face $face, private Image $image)
+    {
+    }
+
+    public function setBorderColor(int $color): static
+    {
+        $this->borderColor = $color;
+
+        return $this;
+    }
+
+    public function draw(): Image
+    {
+        /** @var FaceData $face */
+        foreach ($this->face->detect() as $face) {
+            $vertices = $face->getBounds();
+
+            if ($vertices) {
+                $x1 = $vertices[0]->getX();
+                $y1 = $vertices[0]->getY();
+
+                $x2 = $vertices[2]->getX();
+                $y2 = $vertices[2]->getY();
+
+                $this->image->drawRectangle($x1, $y1, $x2, $y2, $this->borderColor);
+            }
+        }
+
+        return $this->image;
+    }
+}
