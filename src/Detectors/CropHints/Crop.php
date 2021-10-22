79d8944a6ed1ede041cd65407c0793599df86cf2
Ahmad Mayahi <ahmad.mayahi@gmail.com>
2021-10-22 16:17:11 +0200

    CropHints

---
+++ src/Detectors/CropHints/Crop.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AhmadMayahi\Vision\Detectors\CropHints;
+
+use AhmadMayahi\Vision\Detectors\CropHints;
+use AhmadMayahi\Vision\Support\Image;
+
+class Crop
+{
+    public function __construct(private CropHints $cropHints, private Image $image)
+    {
+    }
+
+    public function crop(): Image
+    {
+        /** @var \AhmadMayahi\Vision\Data\CropHints $hint */
+        foreach ($this->cropHints->detect() as $hint) {
+            $bounds = $hint->bounds;
+
+            $this->image->cropImage(
+                x: $bounds[0]->x,
+                y: $bounds[0]->y,
+                width: $bounds[2]->x - 1,
+                height: $bounds[2]->y - 1,
+            );
+        }
+
+        return $this->image;
+    }
+}
---
+++ src/Detectors/CropHints/DrawBoxAroundHints.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AhmadMayahi\Vision\Detectors\CropHints;
+
+use AhmadMayahi\Vision\Detectors\CropHints;
+use AhmadMayahi\Vision\Enums\Color;
+use AhmadMayahi\Vision\Support\Image;
+
+class DrawBoxAroundHints
+{
+    public function __construct(private CropHints $cropHints, private Image $image, private int $borderColor = Color::GREEN)
+    {
+    }
+
+    public function draw(): Image
+    {
+        /** @var \AhmadMayahi\Vision\Data\CropHints $hint */
+        foreach ($this->cropHints->detect() as $hint) {
+            $bounds = $hint->bounds;
+
+            $this->image->drawPolygon([
+                $bounds[0]->x,
+                $bounds[0]->y,
+                $bounds[1]->x,
+                $bounds[1]->y,
+                $bounds[2]->x,
+                $bounds[2]->y,
+                $bounds[3]->x,
+                $bounds[3]->y,
+            ], 4, $this->borderColor);
+        }
+
+        return $this->image;
+    }
+}
