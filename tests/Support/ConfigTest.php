bd9f4652cfff445baa238b577d47b64ea4ec3e2d
Ahmad Mayahi <ahmad.mayahi@gmail.com>
2021-10-22 16:18:53 +0200

    Test Config

---
+++ tests/Support/ConfigTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AhmadMayahi\Vision\Tests\Support;
+
+use AhmadMayahi\Vision\Config;
+use AhmadMayahi\Vision\Exceptions\ConfigException;
+use AhmadMayahi\Vision\Tests\TestCase;
+
+final class ConfigTest extends TestCase
+{
+    /** @test */
+    public function it_should_build_connector_config(): void
+    {
+        $config = (new Config())
+            ->setCredentials($this->googleServiceAccount())
+            ->setCredentialsConfig(['useJwtAccessWithScope' => false])
+            ->setClientConfig('myconfig')
+            ->setApiEndpoint('https://vision.mayahi.net')
+            ->setTransport('grpc')
+            ->disableRetries();
+
+        $expected = [
+            'credentials' => $this->googleServiceAccount(),
+            'credentialsConfig' => [
+                'useJwtAccessWithScope' => false,
+            ],
+            'clientConfig' => 'myconfig',
+            'apiEndpoint' => 'https://vision.mayahi.net',
+            'transport' => 'grpc',
+            'disableRetries' => true,
+        ];
+
+        $this->assertEquals($expected, $config->getConnectorConfig());
+    }
+
+    /** @test */
+    public function it_should_fail_if_no_credentials_found()
+    {
+        $this->expectException(ConfigException::class);
+
+        $this->expectExceptionMessage('Could not find the credentials in config!');
+
+        (new Config())->getConnectorConfig();
+    }
+}
