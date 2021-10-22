f9c76f94665fa34b918cb8aefbfdb0823feb63e3
Ahmad Mayahi <ahmad.mayahi@gmail.com>
2021-10-22 16:19:08 +0200

    Update TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,8 @@ namespace AhmadMayahi\Vision\Tests;
 
 use AhmadMayahi\Vision\Config;
 use AhmadMayahi\Vision\Support\File;
+use Google\Protobuf\Internal\RepeatedField as RepeatedFieldVision;
+use Google\Protobuf\Internal\RepeatedFieldIter;
 use PHPUnit\Framework\MockObject\MockObject;
 use stdClass;
 
@@ -12,7 +14,7 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
     /** @after */
     public function tempCleanUp(): void
     {
-        $tempPath = __DIR__ . DIRECTORY_SEPARATOR . 'files/temp' . DIRECTORY_SEPARATOR;
+        $tempPath = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'files/temp' . DIRECTORY_SEPARATOR;
 
         $files = array_diff(scandir($tempPath), ['.', '..', '.gitignore']);
 
@@ -21,13 +23,18 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
         }
     }
 
+    protected function googleServiceAccount(): string
+    {
+        return dirname(__DIR__) . DIRECTORY_SEPARATOR . 'files/service-account.json';
+    }
+
     protected function getConfig(): Config
     {
         return (new Config())
-            ->setCredentials(__DIR__ . DIRECTORY_SEPARATOR . 'files/service-account.json');
+            ->setCredentials($this->googleServiceAccount());
     }
 
-    public function mockIterator(MockObject $iteratorMock, array $items): MockObject
+    private function mockIterator(MockObject $iteratorMock, array $items): MockObject
     {
         $iteratorData = new stdClass();
         $iteratorData->array = $items;
@@ -88,21 +95,33 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
 
     protected function getFilePathname(string $file = null): string
     {
-        return __DIR__ . DIRECTORY_SEPARATOR . 'files/input/' . ($file ?? 'google-guys.jpg');
+        return dirname(__DIR__) . DIRECTORY_SEPARATOR . 'files/input/' . ($file ?? 'larry-and-sergey.jpg');
     }
 
     protected function getTempDir(string $file = null): string
     {
-        return __DIR__ . DIRECTORY_SEPARATOR . 'files/temp/' . $file;
+        return dirname(__DIR__) . DIRECTORY_SEPARATOR . 'files/temp/' . $file;
     }
 
-    protected function getFile(): File
+    protected function getFile(string $filename = null): File
     {
-        return new File($this->getFilePathname(), $this->getTempDir());
+        return new File($this->getFilePathname($filename), $this->getTempDir());
     }
 
-    protected function createIterator(array $array): Iter
+    protected function createRepeatedField(array $array): RepeatedFieldVision
     {
-        return new Iter($array);
+        return new RepeatedField($array);
+    }
+
+    protected function createRepeatedFieldIter($container)
+    {
+        $repeatedFieldIter = $this->createMock(RepeatedFieldIter::class);
+        $repeatedField = $this->createMock(RepeatedFieldVision::class);
+        $repeatedField
+            ->expects($this->once())
+            ->method('getIterator')
+            ->willReturn($this->mockIterator($repeatedFieldIter, $container));
+
+        return $repeatedField;
     }
 }
