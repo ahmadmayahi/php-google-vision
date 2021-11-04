43510ba329cbd625912ba14271426434cce01ed6
Ahmad Mayahi <ahmad.mayahi@gmail.com>
2021-11-04 18:13:48 +0100

    Tests

--- tests/Traits/ArrayableTest.php
+++ tests/Traits/ArrayableTest.php
@@ -4,23 +4,36 @@ namespace AhmadMayahi\Vision\Tests\Traits;
 
 use AhmadMayahi\Vision\Tests\TestCase;
 use AhmadMayahi\Vision\Traits\Arrayable;
+use Generator;
 
 class ArrayableTest extends TestCase
 {
     /** @test */
     public function it_should_return_array_from_iterator(): void
     {
-        $this->assertSame(range(1, 10), (new Data())->asArray());
+        $data = range(1, 10);
+
+        $this->assertSame($data, (new ArrayData($data))->asArray());
+    }
+
+    /** @test */
+    public function it_should_return_empty_array_if_iterator_returns_is_empty(): void
+    {
+        $this->assertEmpty((new ArrayData([]))->asArray());
     }
 }
 
-class Data
+class ArrayData
 {
     use Arrayable;
 
-    public function detect(): mixed
+    public function __construct(private array $data)
     {
-        foreach (range(1, 10) as $item) {
+    }
+
+    public function detect(): Generator
+    {
+        foreach ($this->data as $item) {
             yield $item;
         }
     }
--- tests/Traits/JsonableTest.php
+++ tests/Traits/JsonableTest.php
@@ -4,23 +4,37 @@ namespace AhmadMayahi\Vision\Tests\Traits;
 
 use AhmadMayahi\Vision\Tests\TestCase;
 use AhmadMayahi\Vision\Traits\Jsonable;
+use Generator;
 
 class JsonableTest extends TestCase
 {
     /** @test */
     public function it_should_return_json_from_iterator(): void
     {
-        $this->assertSame(json_encode(range(1, 10)), (new Data())->asJson());
+        $data = range(1, 10);
+
+        $this->assertJson(json_encode($data), (new JsonData($data))->asJson());
     }
+
+    /** @test */
+    public function it_should_return_empty_json_if_iterator_returns_is_empty(): void
+    {
+        $this->assertJson(json_encode([]), (new JsonData([]))->asJson());
+    }
+
 }
 
-class Data
+class JsonData
 {
     use Jsonable;
 
-    public function detect(): mixed
+    public function __construct(private array $data)
     {
-        foreach (range(1, 10) as $item) {
+    }
+
+    public function detect(): Generator
+    {
+        foreach ($this->data as $item) {
             yield $item;
         }
     }
